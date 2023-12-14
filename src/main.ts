a80105f167fb02e34f97f7b5f3dd640a3bf92cc1
Mayank Kushal <mayankkushal26@gmail.com>
2023-12-14 15:31:36 +0530

    add transaction modal and protocol

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,19 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-	TFile,
-} from "obsidian";
+import { App, Modal, Plugin, PluginSettingTab, Setting } from "obsidian";
 import { parser as ptaLangParser } from "src/parser/ptaLangParser";
 import { parser } from "src/parser/ptaParser";
 import { BalanceView } from "./ui/BalanceView";
+import { createTransactionForm } from "./ui/TransactionForm";
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PtaPluginSettings {
+	ledgerPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
+const DEFAULT_SETTINGS: PtaPluginSettings = {
+	ledgerPath: "Ledger.md",
 };
 
-const ptaCode = `
-2023-08-10 "Starting Balance"
- Assets:Banking:HDFC 1000.00INR
- Assets:Banking:Axis 2000.00INR
-
-2023-08-10 "Lunch"
- Expenses:Food:Lunch 110.00INR
- Assets:Banking:HDFC
-
-2023-08-10 "Dinner"
- Expenses:Food:Dinner 150.00INR
- Assets:Banking:HDFC
-
-2023-08-11 "C"
- Expenses:Cigg 150INR
- Assets:Banking:HDFC
-`;
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PtaPlugin extends Plugin {
+	settings: PtaPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -49,63 +23,19 @@ export default class MyPlugin extends Plugin {
 			"dice",
 			"Sample Plugin",
 			async (evt: MouseEvent) => {
-				const { vault } = this.app;
-
-				const ledger = vault.getAbstractFileByPath("Ledger.md");
-
-				if (ledger instanceof TFile) {
-					const ledgerContent = await vault.read(ledger);
-					const controller = parser(ledgerContent);
-				}
+				new TransactionModal(this.app, this).open();
 			}
 		);
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
+		// Add protocol to open transaction modal
+		this.registerObsidianProtocolHandler("simple-pta", () => {
+			new TransactionModal(this.app, this).open();
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new PtaSettingTab(this.app, this));
 
 		this.registerMarkdownCodeBlockProcessor(
 			"pta",
@@ -113,7 +43,9 @@ export default class MyPlugin extends Plugin {
 				const query = ptaLangParser(source.trim());
 				const { vault } = this.app;
 
-				const ledger: any = vault.getAbstractFileByPath("Ledger.md");
+				const ledger: any = vault.getAbstractFileByPath(
+					this.settings.ledgerPath
+				);
 
 				const ledgerContent = await vault.cachedRead(ledger);
 				const controller = parser(ledgerContent);
@@ -140,14 +72,53 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class TransactionModal extends Modal {
+	plugin: PtaPlugin;
+
+	constructor(app: App, plugin: PtaPlugin) {
 		super(app);
+		this.plugin = plugin;
 	}
+
+	formatTransaction(
+		date: string,
+		description: string,
+		accounts: { account?: string; amount?: number }[]
+	): string {
+		let output = `${date} "${description}"\n`;
+
+		for (const { account, amount } of accounts) {
+			if (account) {
+				output += ` ${account}`;
+
+				if (amount) {
+					output += ` ${amount}INR`;
+				}
+			}
+			output += "\n";
+		}
+
+		return output;
+	}
+
+	handleFormSubmit = async (
+		date: string,
+		description: string,
+		accounts: { account?: string; amount?: number }[]
+	) => {
+		const ledger: any = this.app.vault.getAbstractFileByPath(
+			this.plugin.settings.ledgerPath
+		);
+
+		this.app.vault.append(
+			ledger,
+			this.formatTransaction(date, description, accounts)
+		);
+	};
 
 	onOpen() {
 		const { contentEl } = this;
-		contentEl.setText("Woah!");
+		createTransactionForm(contentEl, this.handleFormSubmit, this.app);
 	}
 
 	onClose() {
@@ -156,10 +127,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class PtaSettingTab extends PluginSettingTab {
+	plugin: PtaPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PtaPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -169,17 +140,14 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
+		new Setting(containerEl).setName("Ledger Path").addText((text) =>
+			text
+				.setPlaceholder("Ledger.md")
+				.setValue(this.plugin.settings.ledgerPath)
+				.onChange(async (value) => {
+					this.plugin.settings.ledgerPath = value;
+					await this.plugin.saveSettings();
+				})
+		);
 	}
 }
---
+++ src/ui/TransactionForm.ts
@@ -0,0 +1,172 @@
+import { PopoverSuggest } from "obsidian";
+
+const ALL_BOOKS = [
+	{
+		title: "How to Take Smart Notes",
+		author: "Sönke Ahrens",
+	},
+	{
+		title: "Thinking, Fast and Slow",
+		author: "Daniel Kahneman",
+	},
+	{
+		title: "Deep Work",
+		author: "Cal Newport",
+	},
+];
+
+export class ExampleModal extends PopoverSuggest<any> {
+	getSuggestions() {
+		return this.app.vault.getMarkdownFiles();
+	}
+
+	open() {
+		console.log("opne called");
+		super.open();
+	}
+
+	renderSuggestion(value: any, el: HTMLElement) {
+		el.appendText(value.name);
+	}
+
+	selectSuggestion(value: any, evt: MouseEvent | KeyboardEvent) {
+		console.log("selectSuggestion", value);
+	}
+}
+
+export function createTransactionForm(
+	el: HTMLElement,
+	onSubmit: Function,
+	app: any
+): void {
+	const form = document.createElement("form");
+	form.className = "max-w-md mx-auto p-4 bg-transparent shadow-md rounded-md";
+
+	// Date field
+	const dateLabel = document.createElement("label");
+	dateLabel.className = "block text-sm font-medium";
+	dateLabel.textContent = "Date";
+	const dateInput = document.createElement("input");
+	dateInput.type = "date";
+	dateInput.className = "mt-1 p-2 block w-full rounded-md border-gray-300";
+	dateInput.value = new Date().toISOString().split("T")[0]; // Default to today
+	dateLabel.appendChild(dateInput);
+	form.appendChild(dateLabel);
+
+	// Description Field
+	const descriptionLabel = document.createElement("label");
+	descriptionLabel.className = "block text-sm font-medium";
+	descriptionLabel.textContent = "Description";
+	const descriptionInput = document.createElement("input");
+	descriptionInput.type = "text";
+	descriptionInput.className =
+		"mt-1 p-2 block w-full rounded-md border-gray-300";
+	descriptionLabel.appendChild(descriptionInput);
+	form.appendChild(descriptionLabel);
+
+	// Transactions fields
+	const transactionsLabel = document.createElement("label");
+	transactionsLabel.className = "block mt-4 font-medium";
+	transactionsLabel.textContent = "Accounts";
+	form.appendChild(transactionsLabel);
+
+	// Divider
+	const divider = document.createElement("hr");
+	divider.className = "my-2";
+	form.appendChild(divider);
+
+	const transactionsContainer = document.createElement("div");
+	transactionsContainer.className = "grid grid-cols-2 gap-4";
+
+	// Function to add a new row
+	function addRow() {
+		const accountLabel = document.createElement("label");
+		accountLabel.className = "block text-sm font-medium";
+		accountLabel.textContent = "Account";
+		const accountInput = document.createElement("input");
+		accountInput.type = "text";
+		accountInput.className =
+			"accountInput mt-1 p-2 block w-full rounded-md border-gray-300";
+
+		const modal = new ExampleModal(app);
+		accountInput.addEventListener("focus", (event) => {
+			modal.open();
+		});
+
+		accountLabel.appendChild(accountInput);
+		const amountLabel = document.createElement("label");
+		amountLabel.className = "block text-sm font-medium";
+		amountLabel.textContent = "Amount";
+		const amountInput = document.createElement("input");
+		amountInput.type = "number";
+		amountInput.className =
+			"amountInput mt-1 p-2 block w-full rounded-md border-gray-300";
+		amountLabel.appendChild(amountInput);
+
+		transactionsContainer.appendChild(accountLabel);
+		transactionsContainer.appendChild(amountLabel);
+	}
+
+	// Initial rows
+	for (let i = 0; i < 2; i++) {
+		addRow();
+	}
+
+	form.appendChild(transactionsContainer);
+
+	// Button Container
+	const buttonContainer = document.createElement("div");
+	buttonContainer.className = "flex justify-between content-center";
+
+	// Plus button to add more rows
+	const plusButton = document.createElement("button");
+	plusButton.type = "button";
+	plusButton.className =
+		"mt-2 bg-green-500 text-white p-2 rounded-md hover:bg-green-600";
+	plusButton.textContent = "+ Account";
+	plusButton.addEventListener("click", addRow);
+	buttonContainer.appendChild(plusButton);
+
+	// Submit button
+	const submitButton = document.createElement("button");
+	submitButton.type = "submit";
+	submitButton.className =
+		"mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600";
+	submitButton.textContent = "Submit";
+	buttonContainer.appendChild(submitButton);
+
+	form.appendChild(buttonContainer);
+
+	// Event listener for form submission
+	form.addEventListener("submit", (event) => {
+		event.preventDefault(); // Prevent the default form submission behavior
+
+		// Extract values from the form elements
+		const dateValue = dateInput.value;
+		const descriptionValue = descriptionInput?.value || "hello";
+
+		const transactionsData = [];
+		const accountInputs = form.querySelectorAll(".accountInput");
+		const amountInputs = form.querySelectorAll(".amountInput");
+
+		for (let i = 0; i < accountInputs.length; i++) {
+			const account = (accountInputs[i] as HTMLInputElement).value;
+			const amount = parseFloat(
+				(amountInputs[i] as HTMLInputElement).value
+			);
+
+			transactionsData.push({ account, amount });
+		}
+
+		// Do something with the extracted data
+		console.log("Submitted Date:", dateValue);
+		console.log("Submitted Transactions Data:", transactionsData);
+
+		onSubmit(dateValue, descriptionValue, transactionsData);
+
+		// Reset the form or perform additional actions as needed
+		form.reset();
+	});
+
+	el.appendChild(form);
+}
